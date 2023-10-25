2f829ced05c9487e852264770cd93917ad3765af
AShaww <71328022+AShaww@users.noreply.github.com>
2023-10-25 23:11:13 +0100

    Handle errors for newIssues, update styles on issue

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,40 +1,52 @@
 'use client';
 
-import React from 'react'
-import { Button, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react'
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, SubmitHandler } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
-
+import z from 'zod';
 
 interface IssueForm {
   title: string;
   description: string;
 }
 
-const NewIssuePage = () => { 
+const createIssueSchema = z.object({
+  title: z.string().min(1, 'Title is required').max(255),
+  description: z.string().min(1, 'Description is required')
+});
+
+
+const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
-  
+  const [error, setError] = useState('');
 
-  const onSubmit = async (data: IssueForm) => {
+  const onSubmit: SubmitHandler<IssueForm> = async (data) => {
     try {
+      createIssueSchema.parse(data);
       await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/issues/new`, data);
       router.push('/issues');
     } catch (error) {
-      console.error('Error creating the issue:', error);
-      // We are using fetch elsewhere, need to decide which to use going forward
+      setError('An unexpected error occured.');
     }
   };
 
   return (
-    <>
-      <form className='max-w-xl space-y-3'
+    <div className='max-w-xl '>
+      { error && 
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      }
+      <form 
+        className='space-y-3' 
         onSubmit={handleSubmit(onSubmit)}>
         <TextField.Root>
-            <TextField.Input size='3' placeholder='Title' {...register('title')}/>
+          <TextField.Input size='3' placeholder='Title' {...register('title')}/>
         </TextField.Root>
         <Controller
           name='description'
@@ -43,8 +55,8 @@ const NewIssuePage = () => {
         />
         <Button>Submit new issue</Button>
       </form>
-    </>
-  )
+    </div>
+  );
 }
 
 export default NewIssuePage
