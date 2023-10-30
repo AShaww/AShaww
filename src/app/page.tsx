1b47ae2b115e0ba3696be625c59cdd599f4a0323
AShaww <71328022+AShaww@users.noreply.github.com>
2023-10-30 14:32:05 +0000

    updated homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,49 @@
+
 import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
+import Image from "next/image";
 
 export default function Home() {
+  const imageWidth = 220;
+  const imageHeight = 220; 
+
   return (
-    <main>
-      <section>
-        <h1>Hello world</h1>
-        <Link href="/users">Users</Link>
-        <ProductCard />
-      </section>
-    </main>
+    <>
+    <main className="flex flex-col items-center justify-center min-h-screen">
+        <div className="-mt-52"> 
+          <Image
+            src={`https://via.placeholder.com/${imageWidth}x${imageHeight}`}
+            alt="Placeholder"
+            width={imageWidth}
+            height={imageHeight}
+            className=""
+          />
+        </div>
+        <div className="mt-4">
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#" className="text-gray-600 hover:text-gray-900">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-gray-600 hover:text-gray-900">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="text-gray-600 hover:text-gray-900">
+                <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
   )
 }
